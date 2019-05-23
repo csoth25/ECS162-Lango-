29282e0721e30ae44f31d11080e6078a702d51b7
Terri <tsoth25@gmail.com>
2019-05-23 20:07:03 +0000

    added translation query handler from API phase

--- langoServer.js
+++ langoServer.js
@@ -80,6 +80,74 @@ function queryHandler(req, res, next) {
     }
 }
 
+/*teach your app to answer translation queries, When it gets a URL in this format
+http://server162.site:port/translate?english=example phrase*/
+function translateHandler(req, res, next) {
+    let qObj = req.translate;
+    
+    const myurl = require('url');
+    const adr = req.url;
+    const q = myurl.parse(adr, true);
+    var qdata = q.query;
+    
+    if (qdata.english!= undefined){
+        //Return a HTTP response with an empty body, to let the browswer know everything went well.
+        res.json( {} );
+        var source = qdata.english;
+        
+        // An object containing the data expressing the query to the translate API.
+        // Below, gets stringified and put into the body of an HTTP PUT request.
+        let requestObject =
+        {
+            "source": "en",
+            "target": "es",
+            "q": [
+                  " "
+                  ]
+        }
+        
+        requestObject.q[0] = source;
+        console.log("English phrase: ", requestObject.q[0]);
+        
+        // The call that makes a request to the API
+        // Uses the Node request module, which packs up and sends off
+        // an HTTP message containing the request to the API server
+        APIrequest(
+                   { // HTTP header stuff
+                   url: url,
+                   method: "POST",
+                   headers: {"content-type": "application/json"},
+                   // will turn the given object into JSON
+                   json: requestObject    },
+                   // callback function for API request
+                   APIcallback
+                   );
+        // callback function, called when data is received from API
+        function APIcallback(err, APIresHead, APIresBody) {
+            // gets three objects as input
+            if ((err) || (APIresHead.statusCode != 200)) {
+                // API is not working
+                console.log("Got API error");
+                console.log(APIresBody);
+            } else {
+                if (APIresHead.error) {
+                    // API worked but is not giving you data
+                    console.log(APIresHead.error);
+                } else {
+                    var text = APIresBody.data.translations[0].translatedText;
+                    console.log("In Spanish: ", text);
+                    
+                    console.log("\n\nJSON was:");
+                    console.log(JSON.stringify(APIresBody, undefined, 2));
+                    // print it out as a string, nicely formatted
+                }
+            }
+        } // end callback function
+    }
+    else {
+        next();
+    }
+}
 
 /*Teach server to respond to AJAX queries of the form:
 http://server162.site:52547/store?english=hi&spanish=hola*/
@@ -160,7 +228,8 @@ function fileNotFound(req, res) {
 // put together the server pipeline
 const app = express()
 app.use(express.static('public'));  // can I find a static file? 
-app.get('/query', queryHandler);   // if not, is it a valid query?
+app.get('/query', queryHandler);
+app.get('/translate', translateHandler);
 app.get('/store', storeHandler);
 app.use( fileNotFound );            // otherwise not found
 app.listen(port, function (){console.log('Listening...');} )
